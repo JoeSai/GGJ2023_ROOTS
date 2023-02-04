290c2c91cd16943b266c25c7c47568b54a794101
凯文(王圣贤) <839144482@qq.com>
2023-02-04 20:41:47 +0800

    对话气泡的表现效果

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -7,10 +7,14 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public GameObject WeChatMassage;
+    public GameObject DialogueLeft;
+    public GameObject DialogueRight;
 
     public GameObject SpanPoint_WeChat;
 
     public Button TestButton;
+    public Button TestButton2;
+    public Button TestButton3;
 
     float DelayClose_WeChat = 2.5f;
 
@@ -18,6 +22,8 @@ public class UIManager : MonoBehaviour
     void Start()
     {
         this.TestButton.onClick.AddListener(PushWeChat);
+        this.TestButton2.onClick.AddListener(PushDialogueLeft);
+        this.TestButton3.onClick.AddListener(PushDialogueRight);
     }
 
     // Update is called once per frame
@@ -30,7 +36,10 @@ public class UIManager : MonoBehaviour
     public void PushWeChat()
     {
         GameObject obj = GameObject.Instantiate(this.WeChatMassage, this.SpanPoint_WeChat.transform);
-        obj.transform.DOMoveY(471, 0.4f);
+        obj.transform.DOMoveY(456, 0.4f);
+        
+        obj.GetComponent<wechat>().name.text = "老板";
+        //obj.GetComponent<wechat>().content.text = content;
 
         StartCoroutine(PopWeChat(obj));
     }
@@ -40,19 +49,56 @@ public class UIManager : MonoBehaviour
     {
         yield return new WaitForSeconds(DelayClose_WeChat);
 
-        obj.transform.DOMoveY(671, 0.2f);
+        obj.transform.DOMoveY(656, 0.2f);
 
         yield return new WaitForSeconds(DelayClose_WeChat);
 
         Destroy(obj);
     }
 
-    // 气泡弹出
-    public void PushDialogue()
+    // 左侧气泡弹出
+    public void PushDialogueLeft()
     {
+        GameObject obj = GameObject.Instantiate(this.DialogueLeft, this.SpanPoint_WeChat.transform);
+        obj.transform.DOScale(new Vector3(1, 1, 1), 0.1f);
 
+        //obj.GetComponent<wechat>().content.text = content;
+
+        StartCoroutine(PopDialogLeft(obj));
     }
 
-    // 对话气泡消失
+    // 左侧对话气泡消失
+    IEnumerator PopDialogLeft(GameObject obj)
+    {
+        yield return new WaitForSeconds(DelayClose_WeChat);
 
+        obj.transform.DOScale(new Vector3(0, 0, 0), 0.2f);
+
+        yield return new WaitForSeconds(DelayClose_WeChat);
+
+        Destroy(obj);
+    }
+
+    // 右侧气泡弹出
+    public void PushDialogueRight()
+    {
+        GameObject obj = GameObject.Instantiate(this.DialogueRight, this.SpanPoint_WeChat.transform);
+        obj.transform.DOScale(new Vector3(1, 1, 1), 0.2f);
+
+        //obj.GetComponent<wechat>().content.text = content;
+
+        StartCoroutine(PopDialogRight(obj));
+    }
+
+    // 右侧对话气泡消失
+    IEnumerator PopDialogRight(GameObject obj)
+    {
+        yield return new WaitForSeconds(DelayClose_WeChat);
+
+        obj.transform.DOScale(new Vector3(0, 0, 0), 0.2f);
+
+        yield return new WaitForSeconds(DelayClose_WeChat);
+
+        Destroy(obj);
+    }
 }
