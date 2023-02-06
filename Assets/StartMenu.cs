4c3098a7c9ba11175f93a1878f010244afe37127
达达鸭 <839144482@qq.com>
2023-02-07 02:08:39 +0800

    开始界面的文字演出

--- Assets/StartMenu.cs
+++ Assets/StartMenu.cs
@@ -3,21 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class StartMenu : MonoBehaviour
 {
     public Button BtnStart;
 
+    public Image DesPanel;
+
+    public Text DesText;
+
+    public string fullText = "起初，没有人在意这场灾难\n这不过是一次通宵\n一次应酬\n一个需求的实现\n一家公司的崛起\n直到这常灾难和每个人都息息相关";
+
     // Start is called before the first frame update
     void Start()
     {
-        BtnStart.onClick.AddListener(StartGame);
+        BtnStart.onClick.AddListener(Intro);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Intro()
+    {
+        DesPanel.DOFade(1, 2.0f).OnComplete(() =>
+        {
+            Tweener TypeText = DesText.DOText(fullText, 6.0f);
+            TypeText.SetEase(Ease.Linear);
+            TypeText.OnComplete(() =>
+            {
+                StartGame();
+            });
+        });
     }
 
     public void StartGame()
